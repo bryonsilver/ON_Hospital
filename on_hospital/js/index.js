2e648d39a5119572acc9cafcb962bd426f433a2e
siucrystal <76192739+bryonsilver@users.noreply.github.com>
2022-09-21 16:56:22 +0900

    Onhospital

--- on_hospital/js/index.js
+++ on_hospital/js/index.js
@@ -7,5 +7,71 @@ $(document).ready(function(){
     $('.nav_li').mouseleave(function(){
         $('.n_nav_ul').hide();
     })
+
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }, 
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        loop: true,
+        autoplay: {
+            delay: 3000, // 몇 밀리초마다 새로 들어올지
+        },
+        speed: 500, // duration
+        // direction: 'vertical',
+        effect : 'fade',
+ 
+    });
+    var swiper = new Swiper(".mySwiper3", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }, 
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        loop: true,
+        autoplay: {
+            delay: 3000, // 몇 밀리초마다 새로 들어올지
+        },
+        speed: 500, // duration
+        // direction: 'vertical',
+ 
+    });
+    var swiper = new Swiper(".mySwiper2", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 3000, // 몇 밀리초마다 새로 들어올지
+        },
+        speed: 500, // duration
+        // direction: 'vertical',
+ 
+    });
+
+    var swiper2 = new Swiper(".mySwiper2", {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 3000, // 몇 밀리초마다 새로 들어올지
+        },
+        speed: 500, // duration
+        // direction: 'vertical',
+ 
+    });
     
+    $('.swiper').hover(function(){
+        swiper.autoplay.stop()
+    }, function(){
+        swiper.autoplay.start()
+    })
 })
