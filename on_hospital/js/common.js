b63c32b7ad140be46bdacf605576d81fc8f54baa
siucrystal <76192739+bryonsilver@users.noreply.github.com>
2022-09-22 16:58:02 +0900

    On_hospital

--- on_hospital/js/common.js
+++ on_hospital/js/common.js
@@ -1,30 +1,57 @@
 $(document).ready(function(){
-    /* 새 UI 선택 클릭 이벤트가 발생하면 감춤 처리한 구글 번역 콤보리스트에 선택한 언어를 적용해 변경 이벤트를 발생시키는 코드  */
-	document.querySelector('.translation-links').addEventListener('change',function(event) {
+   
 
-		let el = this.value;
-		console.log("!!"+el);
-		if(el != null){
-			const gtcombo = document.querySelector('.goog-te-combo');
-			if (gtcombo == null) {
-				alert("Error: Could not find Google translate Combolist.");
-				return false;
-			}
-			gtcombo.value = el; // 변경할 언어 적용
-			gtcombo.dispatchEvent(new Event('change')); // 변경 이벤트 트리거
+
+	// nav
+	$('.nav_ul .n_nav_ul').hide();
+    $('.nav_li').mouseover(function(){
+        var submenu = $(this).children('.n_nav_ul')
+        submenu.slideDown();
+    });
+    $('.nav_li').mouseleave(function(){
+        $('.n_nav_ul').hide();
+    })
+
+	// nav 전체보기
+	$('.all_see').click(function(){
+		$('.all_s_tab_box').css({display: 'block'})
+		$('.black_pan').css({display: 'block'})
+	})
+	$('.as_tab_title').click(function(){
+		$('.all_s_tab_box').css({display: 'none'})
+		$('.black_pan').css({display: 'none'})
+	})
+	$('.black_pan').click(function(){
+		$('.all_s_tab_box').css({display: 'none'})
+		$('.black_pan').css({display: 'none'})
+	})
+
+	$(window).resize(function(){
+		var width = window.innerWidth;
+		if(width < 1200){
+			$('.all_s_tab_box').css({display: 'none'})
+			$('.black_pan').css({display: 'none'})
 		}
-		return false;
-	});
+	}).resize();
 
-	function googleTranslateElementInit() {
-		//setCookie('googtrans', '/en',1);
-		new google.translate.TranslateElement({
-			pageLanguage: 'en',
-			includedLanguages: "en,af,sq,ar,ar-EG,ar-SA,ar-AE,bg,zh-TW,zh-CN,hr,cs,da,nl,en-PH,et,fa,fi,fr,de,el,hi,hu,id,it,ja,ko,mk,ms,mn,no,pt,ro,ru,es,sv,th,tr-TR,uk,uz,vi",
-			autoDisplay: true,
-			multilanguagePage: true
-
-		}, 'google_translate_element');
-	}
-	//frequently asked questions
+	//burger click시
+	$('.burger_menu_box').click(function(){
+		$('.first_line').css({transform: 'rotate(-90deg)'})
+		$('.last_line').css({transform: 'rotate(-90deg)'})
+	})
+	$('.burger_menu_box').click(function() {
+		var clicks = $(this).data('clicks');
+		if (clicks) {
+		   // odd clicks
+		   	$('.first_line').css({transform: 'rotate(-90deg)'})
+			$('.last_line').css({transform: 'rotate(-90deg)'})
+			$('.burger_span').css(50,function(){transform: 'rotate(45deg)'})
+		} else {
+		   // even clicks
+		   	$('.first_line').css({transform: 'rotate(0deg)'})
+			$('.last_line').css({transform: 'rotate(0deg)'})
+			$('.burger_span').css(50,function(){transform: 'rotate(0)'})
+		}
+		$(this).data("clicks", !clicks);
+	  });
 })
--- on_hospital/js/index.js
+++ on_hospital/js/index.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-    $('.nav_ul .n_nav_ul').hide();
-    $('.nav_li').mouseover(function(){
-        var submenu = $(this).children('.n_nav_ul')
-        submenu.slideDown();
-    });
-    $('.nav_li').mouseleave(function(){
-        $('.n_nav_ul').hide();
-    })
 
     var swiper = new Swiper(".mySwiper", {
         navigation: {
@@ -43,35 +35,54 @@ $(document).ready(function(){
         // direction: 'vertical',
  
     });
+    
     var swiper = new Swiper(".mySwiper2", {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        centeredSlides: true,
+        slidesPerView: 1.3,
+        spaceBetween: 0,
         loop: true,
         autoplay: {
             delay: 3000, // 몇 밀리초마다 새로 들어올지
         },
         speed: 500, // duration
         // direction: 'vertical',
- 
+    
     });
 
-    var swiper2 = new Swiper(".mySwiper2", {
-        slidesPerView: 2,
+    var swiper = new Swiper(".mySwiper4", {
+        slidesPerView: 5,
         spaceBetween: 0,
-        centeredSlides: true,
         loop: true,
         autoplay: {
             delay: 3000, // 몇 밀리초마다 새로 들어올지
         },
         speed: 500, // duration
         // direction: 'vertical',
- 
+        
     });
+    $(window).resize(function(){
+		var width = window.innerWidth;
+		if(width < 1000){
+			$('.mySwiper4')
+			$('.black_pan').css({display: 'none'})
+		}
+	}).resize();
     
     $('.swiper').hover(function(){
         swiper.autoplay.stop()
     }, function(){
         swiper.autoplay.start()
     })
+    $(window).scroll(  
+        function(){  
+            let box_o_top = $('.semi_black_box').offset().top;
+            let s_top = $(window).scrollTop();
+            //스크롤의 위치가 상단에서 450보다 크면  
+            if($(window).scrollTop() > box_o_top){  
+            /* if(window.pageYOffset >= $('원하는위치의엘리먼트').offset().top){ */  
+                $('semi_box').css({});  
+                //위의 if문에 대한 조건 만족시 fix라는 class를 부여함  
+            } 
+        }  
+    );  
+
 })
